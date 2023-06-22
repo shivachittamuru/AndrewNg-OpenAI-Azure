48e5a482f0787dcbd9392f80f6db6cf7258afffe
ShivaKumarChittamuru <shchitt@microsoft.com>
2023-06-21 22:19:18 -0700

    Added Evaluation code

--- ChatGPT-Applications/utils.py
+++ ChatGPT-Applications/utils.py
@@ -345,6 +345,73 @@ def get_product_by_name(name):
 def get_products_by_category(category):
     return [product for product in products.values() if product["category"] == category]
 
+def get_products_and_category():
+    delimiter = "####"
+    system_message = f"""
+    You will be provided with customer service queries. The customer service query will be delimited with {delimiter} characters.
+    Output a python list of objects, where each object has the following format:
+        'category': <one of Computers and Laptops, \
+        Smartphones and Accessories, \
+        Televisions and Home Theater Systems, \
+        Gaming Consoles and Accessories, 
+        Audio Equipment, Cameras and Camcorders>,
+    OR
+        'products': <a list of products that must be found in the allowed products below>
+
+    Where the categories and products must be found in the customer service query.
+    If a product is mentioned, it must be associated with \
+    the correct category in the allowed products list below.
+    If no products or categories are found, output an \
+    empty list.
+
+    Allowed products: 
+
+    Computers and Laptops category:
+    TechPro Ultrabook
+    BlueWave Gaming Laptop
+    PowerLite Convertible
+    TechPro Desktop
+    BlueWave Chromebook
+
+    Smartphones and Accessories category:
+    SmartX ProPhone
+    MobiTech PowerCase
+    SmartX MiniPhone
+    MobiTech Wireless Charger
+    SmartX EarBuds
+
+    Televisions and Home Theater Systems category:
+    CineView 4K TV
+    SoundMax Home Theater
+    CineView 8K TV
+    SoundMax Soundbar
+    CineView OLED TV
+
+    Gaming Consoles and Accessories category:
+    GameSphere X
+    ProGamer Controller
+    GameSphere Y
+    ProGamer Racing Wheel
+    GameSphere VR Headset
+
+    Audio Equipment category:
+    AudioPhonic Noise-Canceling Headphones
+    WaveSound Bluetooth Speaker
+    AudioPhonic True Wireless Earbuds
+    WaveSound Soundbar
+    AudioPhonic Turntable
+
+    Cameras and Camcorders category:
+    FotoSnap DSLR Camera
+    ActionCam 4K
+    FotoSnap Mirrorless Camera
+    ZoomMaster Camcorder
+    FotoSnap Instant Camera
+
+    Only output the list of objects, with nothing else.
+    """
+    return system_message
+
 
 def read_string_to_list(input_string):
     if input_string is None:
